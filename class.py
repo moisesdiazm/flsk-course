2968a69b3654d95fed5898d445e89730d9cf33ab
Moises Diaz <moises.diazm@outlook.com>
2017-06-05 21:31:23 -0500

    first commit

---
+++ class.py
@@ -0,0 +1,7 @@
+class ClassName(object):
+    """docstring for ClassName"""
+    def __init__(self, name, school):
+        super(ClassName, self).__init__()
+        self.school = school
+        self.name = name
+		
